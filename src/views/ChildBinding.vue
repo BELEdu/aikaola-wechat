20e463d01e6b45ae201d225fc0a0ed68f663b84e
huojinzhao <352782712@qq.com>
2018-01-11 12:01:57 +0800

    feat(ChildBinding): UI、Interaction

--- src/views/ChildBinding.vue
+++ src/views/ChildBinding.vue
@@ -1,19 +1,106 @@
 <template>
-  <div>
-    <span>家长绑定小孩</span>
+  <div class="child-binding">
+    <Group
+      label-width="5em"
+      gutter="15px"
+    >
+      <XInput
+        ref="name"
+        v-model="data.name"
+        title="学员姓名"
+        placeholder="请输入姓名"
+      />
+      <XInput
+        v-model="data.identifier"
+        title="识别码"
+        placeholder="请输入识别码"
+      />
+      <VerificationCode
+        v-model="data.code"
+        telphone="11111"
+        url="www"
+        label-width="5em"
+      />
+    </Group>
+    <XButton
+      class="child-binding__confirm"
+      type="primary"
+      :show-loading="submitLoading"
+      text="确认"
+      @click.native="v_submit"
+    />
   </div>
 </template>
 
 <script>
 /**
  * @desc 家长绑定小孩
+ * @author hjz
  */
+import {
+  formUtils,
+} from '@/mixins';
+
+import {
+  VerificationCode,
+} from '@/components';
+
+import {
+  Group,
+  XButton,
+  XInput,
+} from 'vux';
 
 export default {
-  name: 'ChildBinding',
+  name: 'Signup',
+
+  components: {
+    Group,
+    VerificationCode,
+    XButton,
+    XInput,
+  },
+
+  mixins: [
+    formUtils,
+  ],
+
+  data: () => ({
+    data: {
+      name: '',
+      identifier: '',
+      code: '',
+    },
+
+    rules: {
+      name: '请填写姓名',
+      identifier: '请填写识别码',
+      code: '请输入验证码',
+    },
+
+    submitLoading: false,
+  }),
+
+  methods: {
+    v_submit() {
+      const valid = this.validateForm(this.data, this.rules);
+
+      if (valid) this.submit();
+    },
+
+    submit() {
+      this.$router.push('/center');
+    },
+  },
 };
 </script>
 
 <style lang="less">
+.child-binding {
 
+  .child-binding__confirm {
+    margin-top: 30px;
+    width: calc(~"100% - 30px");
+  }
+}
 </style>
