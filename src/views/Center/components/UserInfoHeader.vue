161da0c689358905fc651cfe9f24d7476c6c4af3
huojinzhao <352782712@qq.com>
2018-01-15 11:29:16 +0800

    feat(UserInfoHeader)

---
+++ src/views/Center/components/UserInfoHeader.vue
@@ -0,0 +1,43 @@
+<template functional>
+  <div
+    class="user-info__header"
+    @click="props.click"
+  >
+    <div class="right-arrow user-info__header-content">
+      <span class="user-info__header-avatar">
+        <img :src="props.data.avatar">
+      </span>
+      <div class="user-info__header-desc">
+        <span>{{props.data.name}}</span>
+        <span>{{props.data.phone}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * @desc    个人中心 - 用户信息 - 个人卡片
+ * @author  hjz
+ */
+
+export default {
+  name: 'UserInfoHeader',
+
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+
+    click: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="less">
+
+</style>
