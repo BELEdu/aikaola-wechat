2457ad829dd053b5347818b905b0f95efe0e56f7
huojinzhao <352782712@qq.com>
2018-01-16 09:21:47 +0800

    feat(components/ThePanel)

---
+++ src/components/ThePanel.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    class="the-panel"
+    @click="() => click && click()"
+  >
+    <div :class="{
+        'right-arrow': rightArrow,
+        'the-panel__content': true,
+      }"
+    >
+      <span class="the-panel__avatar">
+        <img :src="avatar">
+      </span>
+      <div class="the-panel__description">
+        <span>{{name}}</span>
+        <slot name="content"/>
+      </div>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<script>
+/**
+ * @desc    人物信息展示组件
+ * @author  hjz
+ */
+
+export default {
+  name: 'ThePanel',
+
+  props: {
+    avatar: String,
+
+    name: String,
+
+    click: Function,
+
+    rightArrow: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="less">
+@gutter: 15px;
+
+.the-panel {
+  position: relative;
+  padding-top: @gutter;
+  margin-bottom: @gutter;
+}
+
+.the-panel__content {
+  display: flex;
+  padding: @gutter;
+  background-color: #fff;
+}
+
+.the-panel__avatar {
+  flex: none;
+  margin-right: 12px;
+  height: 65px;
+  width: 65px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+}
+
+.the-panel__description {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  line-height: 1;
+  color: @text-color-subsidiary;
+  font-size: @text-size-content;
+
+  span:first-of-type {
+    font-size: @text-size-title;
+    color: @text-color-default;
+  }
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as DefaultContent } from './DefaultContent';
 export { default as SwiperIndicator } from './SwiperIndicator';
+export { default as ThePanel } from './ThePanel';
 export { default as VerificationCode } from './VerificationCode';
