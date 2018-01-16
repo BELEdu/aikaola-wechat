cd019b806074ffd6da08c7a3bef55a4b26a6407d
huojinzhao <352782712@qq.com>
2018-01-16 09:27:11 +0800

    feat(UserChild)

--- src/views/Center/UserChild.vue
+++ src/views/Center/UserChild.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <span>个人中心 - 孩子信息</span>
+  <div class="user-child">
+    <ThePanel
+      v-slide:left="() => slideCourses(1)"
+      v-slide:right="() => slideCourses(-1)"
+      :avatar="data.avatar"
+      :name="data.name"
+    >
+      <span slot="content">
+        课程名称：{{course.name}}
+      </span>
+      <span slot="content">
+        剩余课时：{{course.rest}}小时
+      </span>
+
+      <SwiperIndicator
+        :value="courseIndex"
+        :length="data.courses.length"
+      />
+    </ThePanel>
+
+    <div
+      class="user-child__evaluations"
+    >
+      <UserChildEvaluation
+        v-for="rate in course.rates"
+        :key="rate.date"
+        :data="rate"
+      />
+    </div>
   </div>
 </template>
 
@@ -8,12 +35,84 @@
 /**
  * @desc 个人中心 - 孩子信息
  */
+import { slide } from '@/directives';
+
+import {
+  SwiperIndicator,
+  ThePanel,
+} from '@/components';
+
+import { UserChildEvaluation } from './components';
 
 export default {
   name: 'UserChild',
+
+  components: {
+    SwiperIndicator,
+    ThePanel,
+    UserChildEvaluation,
+  },
+
+  directives: {
+    slide,
+  },
+
+  data: () => ({
+    courseIndex: 0,
+
+    data: {
+      avatar: 'http://img3.imgtn.bdimg.com/it/u=885192807,544676424&fm=27&gp=0.jpg',
+      name: '包着海苔的饭',
+      courses: [
+        {
+          name: '小学五年级奥数基础班',
+          rest: 3,
+          rates: Array(10).fill(
+            {
+              teacher: '肖冰洁',
+              date: '2017-12-24',
+              test: 'balabala...balabala.',
+            },
+          ),
+        },
+        {
+          name: '小学六年级英语基础班',
+          rest: 10,
+          rates: Array(10).fill(
+            {
+              teacher: '南山南',
+              date: '2017-12-24',
+              test: 'balabala...balabala.',
+            },
+          ),
+        },
+      ],
+    },
+  }),
+
+  computed: {
+    course() {
+      return this.data.courses[this.courseIndex];
+    },
+  },
+
+  methods: {
+    slideCourses(step) {
+      this.courseIndex = (this.courseIndex + step) % this.data.courses.length;
+    },
+  },
 };
 </script>
 
 <style lang="less">
+@gutter: 15px;
 
+.user-child-evaluation:not(:last-child) {
+  border-bottom: 1px solid @bd-color-base;
+}
+
+.user-child__evaluations {
+  background-color: #fff;
+  margin-bottom: @gutter;
+}
 </style>
