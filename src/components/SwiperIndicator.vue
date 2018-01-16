791a49e5d25f6fd6169b031ffe10e4f390bcd1e9
huojinzhao <352782712@qq.com>
2018-01-16 09:10:46 +0800

    feat(componensts/SwiperIndicator)

---
+++ src/components/SwiperIndicator.vue
@@ -0,0 +1,58 @@
+<template functional>
+  <div
+    v-if="props.length > 1"
+    class="swiper-indicator"
+  >
+    <span
+      v-for="i in props.length"
+      :key="i"
+      :class="[
+        'swiper-indicator-dot',
+        i - 1 === props.value && 'active'
+      ]"
+    ></span>
+  </div>
+</template>
+
+<script>
+/**
+ * @desc 轮播指示组件
+ */
+
+export default {
+  name: 'SwiperIndicator',
+
+  props: {
+    value: Number,
+
+    length: Number,
+  },
+};
+</script>
+
+<style lang="less">
+@gutter: 15px;
+
+.swiper-indicator {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: @gutter;
+}
+
+.swiper-indicator-dot {
+  height: 6px;
+  width: 6px;
+  margin: 2px;
+  border-radius: 3px;
+  background-color: #d0cdd1;
+}
+
+.swiper-indicator-dot.active {
+  background-color: @primary-color;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-// eslint-disable-next-line
-export { default as VerificationCode } from './VerificationCode'
 export { default as DefaultContent } from './DefaultContent';
+export { default as SwiperIndicator } from './SwiperIndicator';
+export { default as VerificationCode } from './VerificationCode';
