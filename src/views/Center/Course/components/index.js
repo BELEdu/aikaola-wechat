b25a44f19525846a814b0a3a3ce2e442c892b225
zhoumenglin <512648622@qq.com>
2018-01-18 11:53:37 +0800

    feat(Course):课程页结构规划

---
+++ src/views/Center/Course/components/index.js
@@ -0,0 +1,3 @@
+export { default as CourseCalendar } from './CourseCalendar';
+export { default as CourseMask } from './CourseMask';
+export { default as ChildrenSelect } from './ChildrenSelect';
--- src/views/Center/Course/index.vue
+++ src/views/Center/Course/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <span>课程表</span>
+  <div class="course-table">
+
+    <!-- 孩子选择菜单 -->
+    <ChildrenSelect
+      :select-id="currentId"
+      :data="children"
+      @on-change="changeChild"
+    />
+
+    <!-- 日历 -->
+    <CourseCalendar />
 
   </div>
 </template>
@@ -9,9 +18,62 @@
 /**
  * @desc 课程表
  */
+import { CourseCalendar, CourseMask, ChildrenSelect } from './components';
 
 export default {
-  name: 'Course',
+  name: 'course-table',
+
+  components: {
+    CourseCalendar,
+    CourseMask,
+    ChildrenSelect,
+  },
+
+  data() {
+    return {
+      currentId: 1, // 当前选中的孩子id
+
+      children: [
+        {
+          id: 1,
+          name: '张学员',
+          avatar: 'https://static.vux.li/uploader_bg.png',
+        },
+        {
+          id: 2,
+          name: '李学员',
+          avatar: 'https://static.vux.li/uploader_bg.png',
+        },
+        {
+          id: 3,
+          name: '斯琴高娃',
+          avatar: 'https://static.vux.li/uploader_bg.png',
+        },
+        {
+          id: 4,
+          name: '尼古拉夫斯基',
+          avatar: '',
+        },
+        {
+          id: 5,
+          name: '小明',
+          avatar: 'https://static.vux.li/uploader_bg.png',
+        },
+      ],
+
+    };
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    // 改变当前选中的孩子
+    changeChild(id) {
+      this.currentId = id;
+    },
+  },
 };
 </script>
 
