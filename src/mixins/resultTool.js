ce5e76a345e82a3c6c436239a52d053fb4d3ab51
zhoumenglin <512648622@qq.com>
2018-01-18 15:50:18 +0800

    feat(mixin/resultTool):支持参数配置push形式跳转路由

--- src/mixins/resultTool.js
+++ src/mixins/resultTool.js
@@ -8,6 +8,7 @@ export default {
     /**
      * 跳转结果页
      *
+     * @param isReplace   {Boolean} 路由是否replace，默认为true
      * @param obj         {Object} query参数对象
      * @param obj.status  {String} （'error'：失败） 默认值 'success'：成功
      * @param obj.title   {String} 页面标题
@@ -16,12 +17,17 @@ export default {
      * @param obj.time    {Number} 自动跳转的延迟时间（单位：秒），默认值 3
      */
 
-    toResultPage(obj) {
+    toResultPage(obj, isReplace = true) {
       const queryString = Object.keys(obj)
         .map(key => `${key}=${obj[key]}`)
         .join('&');
 
-      this.$router.replace(`/result?${queryString}`);
+      // 路由跳转模式
+      if (isReplace) {
+        this.$router.replace(`/result?${queryString}`);
+      } else {
+        this.$router.push(`/result?${queryString}`);
+      }
     },
   },
 };
