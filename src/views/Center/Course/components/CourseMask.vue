3acce2ad1a45570a15796956039ee744a51b287e
zhoumenglin <512648622@qq.com>
2018-01-25 18:05:58 +0800

    feat(CourseMask):轮播图具体逻辑

--- src/views/Center/Course/components/CourseMask.vue
+++ src/views/Center/Course/components/CourseMask.vue
@@ -1,67 +1,67 @@
 <template>
-  <div
-    v-if="visible"
-    :value="visible"
-    @input="toggleMask"
-    class="mask"
-  >
-    <!-- 遮罩层 -->
+  <div class="mask">
+
+    <!-- 透明点击区域 -->
     <div class="mask__bg" @click="closeMask"></div>
 
-
-    <swiper :options="swiperOption" ref="mySwiper" class="mySwiper">
+    <!-- swuper插件 -->
+    <swiper
+      :options="swiperOption"
+      ref="mySwiper"
+      class="mySwiper"
+    >
       <!-- slides -->
       <swiper-slide
-        v-for="item in 6"
-        :key="item"
+        v-for="(course,index) in courseArray"
+        :key="index"
       >
-        <div class="mask__item">
-          <section class="mask__wrap is-primary">
-            <!-- 标签 -->
-            <div class="mask__tag">待上课</div>
+        <section
+          class="mask__wrap"
+          :class="{[`${theme(course.course_status)}`]:true}"
+        >
+          <!-- 标签 -->
+          <div class="mask__tag">{{course.course_status_name}}</div>
 
-            <!-- 课序 -->
-            <div class="mask__index">第二节课</div>
+          <!-- 课序 -->
+          <div class="mask__index">第 {{course.sort_value}} 节课</div>
 
-            <!-- 图标 -->
-            <div class="text-center">
-              <svg><use :xlink:href="`#${svgId(1)}`"/></svg>
-            </div>
+          <!-- 图标 -->
+          <div class="text-center">
+            <svg><use :xlink:href="`#${svgId(course.course_status)}`"/></svg>
+          </div>
 
-            <!-- 课程信息 -->
-            <ul class="mask__info">
-              <li>
-                <b>上课时间：</b>
-                <span>2018-01-24（星期六）<br>10:00-12:00</span>
-              </li>
-              <li>
-                <b>上课科目：</b>
-                <span>数学</span>
-              </li>
-              <li>
-                <b>教师姓名：</b>
-                <span>肖冰洁</span>
-              </li>
-              <li>
-                <b>上课内容：</b>
-                <p class="ellipsis">数与式、有理数....函数数与式、有理数....函数</p>
-              </li>
-            </ul>
+          <!-- 课程信息 -->
+          <ul class="mask__info">
+            <li>
+              <b>上课时间：</b>
+              <span>
+                {{course.week}}
+                <br>{{course.course_start_end}}
+              </span>
+            </li>
+            <li>
+              <b>上课科目：</b>
+              <span>{{course.subject}}</span>
+            </li>
+            <li>
+              <b>教师姓名：</b>
+              <span>{{course.teacher_name}}</span>
+            </li>
+            <li>
+              <b>上课内容：</b>
+              <p class="ellipsis">{{course.wechat_chapter_name}}</p>
+            </li>
+          </ul>
 
-            <!-- 查看更多 -->
-            <router-link
-              class="mask__more"
-              :to="`/center/course/${studentId}/10086`"
-            >查看更多</router-link>
+          <!-- 查看更多 -->
+          <router-link
+            v-if="showMore(course.course_status)"
+            class="mask__more"
+            :to="`/center/course/${studentId}/10086`"
+          >查看更多</router-link>
 
-          </section>
-        </div>
+        </section>
       </swiper-slide>
-      <!-- Optional controls -->
-      <div class="swiper-pagination"  slot="pagination"></div>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-      <div class="swiper-scrollbar"   slot="scrollbar"></div>
     </swiper>
 
   </div>
@@ -70,6 +70,7 @@
 <script>
 
 import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import store from '@/store';
 import '@/assets/swiper.css';
 
 export default {
@@ -81,27 +82,28 @@ export default {
   },
 
   props: {
-    visible: {
-      type: Boolean,
-      required: true,
-    },
     studentId: {
       type: Number,
       required: true,
     },
-    // data: {
-    //   type: Array,
-    //   required: true,
-    // },
+    course: {
+      type: Object,
+      required: true,
+    },
   },
 
   data() {
     return {
-      // 所有的参数同 swiper 官方 api 参数
+      /**
+       * @description swiper参数配置
+       * 所有的参数同 swiper 官方 api 参数
+       * http://www.swiper.com.cn/api/index.html
+       * https://github.com/surmon-china/vue-awesome-swiper
+       */
       swiperOption: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        mousewheel: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 20,
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
@@ -111,28 +113,33 @@ export default {
   },
 
   computed: {
-    swiper() {
-      return this.$refs.mySwiper.swiper;
+    // 当前月份所有课程组成的数组
+    courseArray() {
+      return this.course.data.reduce((acc, course) => [...acc, ...course.data], []) || [];
     },
   },
 
   mounted() {
-      // current swiper instance
-      // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
-      // eslint-disable-next-line
-    console.log('this is current swiper instance object', this.swiper);
-    this.swiper.slideTo(3, 1000, false);
   },
 
   methods: {
-    // 监听遮罩层的显示/隐藏
-    toggleMask(val) {
-      this.$emit('update:visible', val);
+    // 关闭弹窗
+    closeMask() {
+      store.commit('updateMask', false);
     },
 
-    // 关闭遮罩层
-    closeMask() {
-      this.$emit('update:visible', false);
+    // 是否可以展示 “查看更多”
+    showMore(status) {
+      switch (status) {
+        case 1:
+          return true;
+        case 2:
+          return true;
+        case 3:
+          return true;
+        default:
+          return false;
+      }
     },
 
     // 图标id
@@ -146,6 +153,20 @@ export default {
           return 'course-comment';
         default:
           return 'course-error';
+      }
+    },
+
+    // 卡片主题
+    theme(status) {
+      switch (status) {
+        case 1:
+          return 'is-primary';
+        case 2:
+          return 'is-success';
+        case 3:
+          return 'is-warning';
+        default:
+          return 'is-error';
       }
     },
   },
@@ -167,12 +188,12 @@ export default {
   }
 }
 
-.is-default {
-  .colormix(@text-color-subsidiary);
-}
-
 .is-primary {
   .colormix(@primary-color);
+}
+
+.is-success {
+  .colormix(@success-color);
 }
 
 .is-warning {
@@ -185,30 +206,24 @@ export default {
 
 .mask {
   position: fixed;
+  z-index: 502;
+  align-items: center;
+  display: flex;
   top: 0;
-  bottom: 0;
+  bottom:0;
   left: 0;
   right: 0;
-  display: flex;
-  align-items: center;
 
   &__bg {
     position: absolute;
     height: 100%;
     width: 100%;
-    background-color:rgba(0, 0, 0, 0.5);
-  }
-
-  &__item {
-    padding: 0 10px;
-    height:@item-height;
-    width: @item-width;
   }
 
   &__wrap {
     position: relative;
-    height:100%;
-    width:100%;
+    height:@item-height;
+    width: @item-width;
     background-color: #fff;
     border-radius: 5px;
     overflow: hidden;
@@ -272,8 +287,12 @@ export default {
 }
 
 .mySwiper {
-  position: relative;
-  width: 100%;
+  // position: relative;
+  // width: 100%;
   // background-color: red;
+
+  .swiper-slide {
+    width: @item-width;
+  }
 }
 </style>
